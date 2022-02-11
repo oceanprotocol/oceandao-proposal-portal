63fc21c4ed34bba48112b4ad5c27445b85d64323
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2022-02-11 13:57:33 -0800

    Reorganized fields, quickly added *required w/o a library, and cut off last page for now.

--- frontend/src/pages/SubmitProposal.svelte
+++ frontend/src/pages/SubmitProposal.svelte
@@ -8,9 +8,8 @@
   let part = 0;
 
   const partTitles = [
-    "Part 1 - Proposal Submission (Mandatory)",
-    "Part 2 - Team",
-    "Part 3 - Proposal Details (Recommended)",
+    "Part 1 - Proposal Details",
+    "Part 2 - Team Details",
   ];
 
   let fieldsPart0 = [
@@ -21,41 +20,6 @@
       required: true,
       wrong: false,
     },
-    {
-      type: "text",
-      title: "Proposal in one sentence",
-      bindValue: "oneLiner",
-      required: true,
-      wrong: false,
-    },
-    {
-      type: "largeText",
-      title: "Grant Deliverables",
-      bindValue: "grantDeliverables",
-      required: true,
-      placeHolder: `__(Grant Deliverable 1)__
-__(Grant Deliverable 2)__
-__(Grant Deliverable 3)__`,
-      wrong: false,
-    },
-    {
-      type: "largeText",
-      title: "Project Description",
-      bindValue: "projectDescription",
-      required: true,
-      placeHolder: "Description of the project and what problem is it solving",
-      wrong: false,
-    },
-    {
-      type: "largeText",
-      title: "Value add criteria description",
-      bindValue: "valueAddCriteria",
-      required: true,
-      placeHolder:
-        "Description of how the project adds value to Ocean ecosystem",
-      wrong: false,
-    },
-
     {
       type: "optionSelect",
       title: "Project Category",
@@ -87,7 +51,7 @@ __(Grant Deliverable 3)__`,
     },
     {
       type: "optionSelect",
-      title: "Are you applying for an Earmark?",
+      title: "What Earmark are you applying to?",
       bindValue: "projectEarmark",
       wrong: false,
       required: true,
@@ -98,7 +62,7 @@ __(Grant Deliverable 3)__`,
         },
         {
           value: "newprojectoutreach",
-          text: "New Project Outreach / community / spread awarenes",
+          text: "New Project Outreach",
         },
         {
           value: "coretech",
@@ -112,6 +76,14 @@ __(Grant Deliverable 3)__`,
     },
     {
       type: "largeText",
+      title: "Project Description",
+      bindValue: "projectDescription",
+      required: true,
+      placeHolder: "Description of the project and the problem it's solving",
+      wrong: false,
+    },
+    {
+      type: "largeText",
       title: "What is the final product?",
       bindValue: "finalProduct",
       placeHolder: "1-2 sentences describing the final product",
@@ -120,13 +92,39 @@ __(Grant Deliverable 3)__`,
     },
     {
       type: "text",
-      title: "Funding Requested (USD)s",
+      title: "Proposal in one sentence",
+      bindValue: "oneLiner",
+      required: true,
+      wrong: false,
+    },
+    {
+      type: "largeText",
+      title: "Grant Deliverables",
+      bindValue: "grantDeliverables",
+      required: true,
+      placeHolder: `__(Grant Deliverable 1)__
+__(Grant Deliverable 2)__
+__(Grant Deliverable 3)__`,
+      wrong: false,
+    },
+    {
+      type: "largeText",
+      title: "Value-Add criteria description",
+      bindValue: "valueAddCriteria",
+      required: true,
+      placeHolder:
+        "Description of how the project adds value to Ocean ecosystem",
+      wrong: false,
+    },
+    {
+      type: "text",
+      title: "Funding Requested (USD)",
       bindValue: "fundingRequested",
       wrong: false,
       required: true,
       textFormat: "number",
       importantText:
-        "The amount requested is in USD, but the amount paid is in OCEAN token. The conversion rate is the market price on the given Round's Proposal Due By Deadline. This determines how many OCEAN will be awarded if a proposal is voted to receive a grant.",
+        "The amount requested is in USD, but the amount paid is in OCEAN token. The conversion rate is calculated at Vote End, so payment is completed as quickly as possible. This determines how many OCEAN will be awarded if a proposal is voted to receive a grant.",
     },
     {
       type: "text",
@@ -137,27 +135,6 @@ __(Grant Deliverable 3)__`,
       required: true,
       importantText:
         "Must have minimum 500 OCEAN in wallet to be eligible. This wallet is where you will receive the grant amount if selected",
-    },
-    {
-      type: "text",
-      title: "Team Website (if applicable)",
-      bindValue: "teamWebsite",
-      placeHolder: "URL",
-      wrong: false,
-    },
-    {
-      type: "text",
-      title: "Twitter Website (if applicable)",
-      bindValue: "twitterLink",
-      placeHolder: "URL",
-      wrong: false,
-    },
-    {
-      type: "text",
-      title: "Discord Website (if applicable)",
-      bindValue: "discordLink",
-      placeHolder: "URL",
-      wrong: false,
     },
     {
       type: "text",
@@ -182,6 +159,27 @@ __(Grant Deliverable 3)__`,
       placeHolder: "USA",
       wrong: false,
       required: true,
+    },
+    {
+      type: "text",
+      title: "Team Website",
+      bindValue: "teamWebsite",
+      placeHolder: "URL",
+      wrong: false,
+    },
+    {
+      type: "text",
+      title: "Twitter Website",
+      bindValue: "twitterLink",
+      placeHolder: "URL",
+      wrong: false,
+    },
+    {
+      type: "text",
+      title: "Discord Website",
+      bindValue: "discordLink",
+      placeHolder: "URL",
+      wrong: false,
     },
   ];
 
@@ -210,7 +208,8 @@ Creator of xxx.js the official JavaScript library for xxx
       title: "Advisors",
       bindValue: "advisors",
       rows: 15,
-      placeHolder: `For each Advisor, give their name, role and background. Use the same format as in "Core Team"`,
+      placeHolder: `For each Advisor, give their name, role and background.
+Use the same format as in "Core Team."`,
     },
   ];
 
@@ -224,9 +223,7 @@ Creator of xxx.js the official JavaScript library for xxx
     },
   ];
 
-  let fieldsPart2 = [];
-
-  let fields = [fieldsPart0, fieldsPart1, fieldsPart2];
+  let fields = [fieldsPart0, fieldsPart1];
 
   function next() {
     if (part < 2) {
@@ -263,7 +260,7 @@ Creator of xxx.js the official JavaScript library for xxx
         {#if field.type === "text"}
           <TextField
             bind:value={$proposal[field.bindValue]}
-            title={field.title}
+            title={field.required ? "* " + field.title : field.title}
             placeHolder={field.placeHolder}
             disabled={field.disabled}
             wrong={field.wrong}
@@ -275,7 +272,7 @@ Creator of xxx.js the official JavaScript library for xxx
         {#if field.type === "largeText"}
           <LargeTextField
             bind:value={$proposal[field.bindValue]}
-            title={field.title}
+            title={field.required ? "* " + field.title : field.title}
             placeHolder={field.placeHolder}
             disabled={field.disabled}
             wrong={field.wrong}
@@ -286,7 +283,7 @@ Creator of xxx.js the official JavaScript library for xxx
         {#if field.type === "optionSelect"}
           <OptionSelect
             bind:value={$proposal[field.bindValue]}
-            title={field.title}
+            title={field.required ? "* " + field.title : field.title}
             placeHolder={field.placeHolder}
             disabled={field.disabled}
             wrong={field.wrong}
@@ -295,10 +292,10 @@ Creator of xxx.js the official JavaScript library for xxx
           />
         {/if}
       {/each}
-
+      <p>* Required Fields</p>
       <div class="flex items-center justify-between">
         <div class="flex space-x-2">
-          {#each Array(3) as _, i}
+          {#each Array(2) as _, i}
             <div
               style="width:{i === part
                 ? '40px'
@@ -325,10 +322,10 @@ Creator of xxx.js the official JavaScript library for xxx
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
-            {#if part < 2}
+            {#if part < 1}
               Next
             {/if}
-            {#if part == 2}
+            {#if part == 1}
               Submit Proposal
             {/if}
           </button>
