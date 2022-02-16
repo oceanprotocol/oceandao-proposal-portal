99bfd6d749c3c5e5d7572b20952e338995565f0a
trizin <25263018+trizin@users.noreply.github.com>
2022-02-16 14:14:30 +0300

    `getNonce` is async

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,6 +1,6 @@
 import { SERVER_URI } from "./config";
 
-export const getNonce = (address) => {
+export const getNonce = async (address) => {
   const nonceResponse = await fetch(`${SERVER_URI}/app/nonce/${signer}`);
   const nonce = await nonceResponse.json();
   return nonce;
