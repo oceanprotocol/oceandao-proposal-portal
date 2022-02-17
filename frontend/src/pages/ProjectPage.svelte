705e9114e595788a8775ca201ded71331dba11c2
Norbert <katunanorbert@gmail.com>
2022-02-17 17:05:18 +0200

    updated proposal page

--- frontend/src/pages/ProjectPage.svelte
+++ frontend/src/pages/ProjectPage.svelte
@@ -93,6 +93,9 @@ You can only have 1 proposal per project, for each funding round.`
     align-items: center;
     justify-content: space-between;
   }
+  .detailName, .detailValue{
+    font-size: var(--font-size-normal);
+  }
 </style>
 
 <div class="flex h-screen flex-col justify-center project-container">
@@ -107,11 +110,11 @@ You can only have 1 proposal per project, for each funding round.`
         >
           <div class="details bg-slate-200 py-5 px-5">
             <div class="col-start-4 col-span-2 ...">
-              <span class="detailName font-bold text-xl">Category</span>
+              <span class="detailName font-bold">Category</span>
               <span class="text-lg detailValue">{grantCategory[project.projectCategory]}</span>
             </div>
             <div class="col-start-7 col-span-2 ...">
-              <span class="detailName font-bold text-xl">Creation date</span>
+              <span class="detailName font-bold">Creation date</span>
               <span class="text-lg detailValue">{moment(project.createdAt).format('YYYY-MM-DD')}</span>
             </div>
           </div>
@@ -131,43 +134,4 @@ You can only have 1 proposal per project, for each funding round.`
           />
        {/if}
       </Section>
-
-    <div class="section">
-      <h2 class="text-lg font-bold title">Project Proposals</h2>
-      <div class="text-left bg-slate-50">
-          <div>{pageText.proposalDescription}</div>
-      </div>
-      <div class="proposalsContainer">
-      {#each proposals as proposal}
-        <div class="flex justify-between proposalCard">
-          <span>Round {proposal.round}</span>
-          <div class="proposalCardDescription">
-            <span>{proposal.proposalEarmark}</span>
-            <span>{proposal.proposalValue}</span>
-          </div>
-          <Link
-            class="flex justify-center font-bold text-lg text-black-600 bg-white"
-            to={`/proposal/view/${proposal._id}`}>View</Link
-          >
-        </div>
-        {/each}
-      </div>
-      <div class="flex mt-5 justify-end">
-        <Button
-                text={"Create Proposal"}
-                onclick={() => {
-            location.href = "/proposal/create/" + projectId;
-          }}
-        />
-      </div>
-    </div>
-    <div class="mt-5">
-      <Button
-        text={"Project settings"}
-        onclick={() => {
-          location.href = "/project/edit/" + projectId;
-        }}
-      />
-    </div>
-  </div>
 </div>
