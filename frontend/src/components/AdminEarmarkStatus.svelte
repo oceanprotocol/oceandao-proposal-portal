58617d97aaf34c923a638e060a666eef5552ec39
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2022-02-24 20:52:11 -0800

    Fix undefined.

--- frontend/src/components/AdminEarmarkStatus.svelte
+++ frontend/src/components/AdminEarmarkStatus.svelte
@@ -1,12 +1,13 @@
 <script>
+  import earmarks from '../../src/utils/types/earmark.json'
   export let proposalEarmarkRequest;
 </script>
 
 <div class="earmarkRequest">
-  <div class="justify-center text-center bg-yellow-50 p-4">
-    {#if proposalEarmarkRequest !== ""}
-      Earmark under review...<br/>
+  {#if proposalEarmarkRequest !== "" && proposalEarmarkRequest !== undefined}
+    <div class="justify-center text-center bg-yellow-50 p-4">
+      Core-Tech Earmark under review...<br/>
       Please <a class="underline" href="https://github.com/oceanprotocol/oceandao/wiki/OceanDAO-Tech-Earmarks">receive clearance from Core-Tech WG.</a>
-    {/if}
-  </div>
+    </div>
+  {/if}
 </div>
