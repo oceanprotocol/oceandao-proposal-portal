e53e19e7f25c8b167fecabeb26555845a1fe1275
Berkay Saglam <25263018+trizin@users.noreply.github.com>
2022-03-15 15:19:07 +0300

    check `0x` (#36)

--- backend/utils/ethers/signature.js
+++ backend/utils/ethers/signature.js
@@ -1,6 +1,7 @@
 const ethers = require("ethers");
 
 function getSigner(hash, message) {
+  if (!hash.startsWith("0x")) hash = "0x" + hash;
   let msg = ethers.utils.verifyMessage(message, hash);
   return msg;
 }
