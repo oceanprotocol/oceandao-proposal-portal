71fdab95188b99eee0705342511fefb0e4de7ead
trizin <25263018+trizin@users.noreply.github.com>
2022-04-07 12:46:30 +0300

    `/number` endpoint

--- backend/routes/round.js
+++ backend/routes/round.js
@@ -4,25 +4,23 @@ const router = express.Router();
 const redis = require("../utils/redis");
 
 router.get("/number", async (req, res) => {
-  redis.get("currentRoundNumber", async (err, roundNumber) => {
-    if (err) {
-      return res.status(400).send(err);
+  let roundNumber = await redis.get("currentRoundNumber");
+  if (!roundNumber) {
+    roundNumber = await getCurrentRoundNumber();
+    if (roundNumber == -1) {
+      return res.status(400).send("No round number found");
     }
-    if (!roundNumber) {
-      roundNumber = await getCurrentRoundNumber();
-      if (roundNumber == -1) {
-        return res.status(400).send("No round number found");
-      }
-      redis.set("currentRoundNumber", roundNumber);
+    redis.set("currentRoundNumber", roundNumber);
 
-      // expire at midnight
-      const todayEnd = new Date().setHours(23, 59, 59, 999);
-      redis.expireAt("currentRoundNumber", parseInt(todayEnd / 1000));
-    }
+    // expire at midnight
+    const todayEnd = new Date().setHours(23, 59, 59, 999);
+    redis.expireAt("currentRoundNumber", parseInt(todayEnd / 1000));
+  }
 
-    return res.status(200).json({
-      roundNumber,
-      success: true,
-    });
+  return res.status(200).json({
+    roundNumber,
+    success: true,
   });
 });
+
+module.exports = router;
