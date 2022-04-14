f496cc98eb6adf420b06ec5937951577c7ecb98a
trizin <25263018+trizin@users.noreply.github.com>
2022-04-14 12:33:24 +0300

    Error handler to catch all unhandled errors

--- backend/app.js
+++ backend/app.js
@@ -26,15 +26,14 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
   console.error(err.message);
 
   // render the error page
   res.status(err.status || 500);
-  res.send("error");
+  res.json({ error: err.message });
 });
 
 app.listen(process.env.PORT, () =>
